0f543180b2e20a02fe92f9fc16b4d1f1e22c26fe
Kirill Safronov <Kirill.tanatos1993@mail.ru>
2025-03-14 22:38:09 +0300

    add config and APScheduler with BaseSettings

---
+++ app/config.py
@@ -0,0 +1,36 @@
+import os
+from typing import List
+
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    ADMIN_IDS: List[int]
+    FORMAT_LOG: str = '{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}'
+    LOG_ROTATION: str = '10 MB'
+    DB_URL: str
+    STORE_URL: str
+    BASE_SITE: str
+    TG_API_SITE: str
+    FRONT_SITE: str
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+        )
+
+    def get_webhook_url(self) -> str:
+        """Возвращаем URL вебхука."""
+        return f'{self.BASE_SITE}/webhook'
+
+    def get_tg_api_url(self) -> str:
+        """Возвращает URL Telegram API."""
+        return f'{self.TG_API_SITE}/bot{self.BOT_TOKEN}'
+
+
+settings = Settings()
+scheduler = AsyncIOScheduler(
+    jobstores={'default': SQLAlchemyJobStore(url=settings.STORE_URL)}
+        )
